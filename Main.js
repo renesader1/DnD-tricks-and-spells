ab8da365056a038eef9c382e55cac8ed5fbbcaef
renesader1 <renesader@outlook.com>
2023-06-11 21:36:48 -0300

    upating

--- Main.js
+++ Main.js
@@ -1,6 +1,6 @@
 async function GetLista(){
     const Lista = await fetch("https://raw.githubusercontent.com/renesader1/DnD-tricks-and-spells/main/Lista.json")
-    let ListaConvertida = await Lista.json() 
+    const ListaConvertida = await Lista.json() 
     AcessaLista(ListaConvertida)
 }
  
